fdaaac3b600679d89a36940ec567c15afdf60315
lijippy@163.com <lijippy@163.com>
2022-06-05 12:26:00 +0800

    update conda build

--- conda/bld.sh
+++ conda/bld.sh
@@ -10,6 +10,11 @@ set -e
 
 mkdir _build
 cd _build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DENABLE_TESTING=OFF
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DENABLE_TESTING=OFF \
+        -DENABLE_BENCHARK=OFF \
+        -DENABLE_PYTHON=OFF
+
 cmake --build .
 cmake --build . --target install
--- conda/bld_py.sh
+++ conda/bld_py.sh
@@ -8,8 +8,4 @@ set -e
 #
 #################################################################
 
-$PYTHON setup.py build_ext bdist_wheel
-whl=`ls dist`
-pip${PY_VER} install --force-reinstall --prefix=$PREFIX dist/${whl}
-rm -r build
-rm -r dist
+$PYTHON setup.py install
